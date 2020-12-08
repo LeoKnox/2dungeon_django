3cfda7df14dcce30c2e61094a9820e20577788ab
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-07 19:02:49 -0800

    Add files via upload

---
+++ rooms-fe/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className = "text-center">
+                <h5>
+                    Dungeon information
+                </h5>
+                <h1> App with React + DJango</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
