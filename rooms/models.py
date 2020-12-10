1fda9c4a6da697da8ce33ebfea45e9ec3c012e0e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-09 20:07:54 -0800

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -8,12 +8,6 @@ class Room(models.Model):
     floor = models.CharField(max_length=5)
     doors = models.ManyToManyField('Door')
 
-    def __str__(self):
-        return self.name
-
 class Door(models.Model):
     door_one = models.IntegerField()
     door_two = models.IntegerField()
-
-    def __str__(self):
-        return self.name
