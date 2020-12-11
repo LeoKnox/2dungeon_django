2f2e1ee60c48e3278e2864ad14db14c338c1ce7a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-10 23:03:32 -0800

    Add files via upload

---
+++ rooms-fe/src/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className = "text-center">
+                <h5>
+                    Django api + Ract.js : Yeah!
+                </h5>
+                <h1>Dungeon React</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ rooms-fe/src/src/components/RoomList.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Table } from "reactstrap";
+import NewRoomModal from "./NewRoomModal";
+
+import ConfirmRemovalModal from "./ConfirmRemovalModal";
+
+class RoomList extends Component {
+    render() {
+        const rooms = this.props.rooms;
+        return (
+            <Table dark>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Width</th>
+                        <th>Height</th>
+                        <th>Floor</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!rooms || rooms.length <= 0 ? (
+                        <tr>
+                            <td colSpan="6" align="center">
+                                <b>Ops, no one here yet</b>
+                            </td>
+                        </tr>
+                    ) : (
+                        rooms.map(room => (
+                            <tr key={room.pk}>
+                                <td>{room.name}</td>
+                                <td>{room.description}</td>
+                                <td>{room.width}</td>
+                                <td>{room.height}</td>
+                                <td>{room.floor}</td>
+                                <td align="center">
+                                    <NewRoomModal
+                                        create={false}
+                                        room={room}
+                                        resetState={this.props.resetState}
+                                    />
+                                    &nbsp;&nbsp;
+                                    <ConfirmRemovalModal
+                                        pk={room.pk}
+                                        resetState={this.props.resetState}
+                                    />
+                                </td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+export default RoomList;
